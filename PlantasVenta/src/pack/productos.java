9c7e2b370a7927daa0e00bbcb6cac7aa6bd55ec3
devin <you@example.com>
2025-02-21 18:50:00 +0100

    Creación de clase Productos y atributoss

---
+++ PlantasVenta/src/pack/productos.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class productos {
+    String nombre;
+    String origen;
+    double precio;
+
+    public productos(String nombre, String origen, double precio) {
+        this.nombre = nombre;
+        this.origen = origen;
+        this.precio = precio;
+    }
+}
